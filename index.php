75c197fb9ad42c0e59ad09e4c07ef49bae7fe1c7
Abdelfettah Akkaoui <abdelfettah.akkaoui.ext@orange.com>
2023-06-20 14:33:28 +0200

    add for NOT FOUND

--- index.php
+++ index.php
@@ -29,6 +29,15 @@ $uri = rawurldecode($uri);
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 
 switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        echo "404 Not Found";
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        echo "405 Method Not Allowed";
+        break;
     case \FastRoute\Dispatcher::FOUND:
         $explode = explode(':', $routeInfo[1]);
 
